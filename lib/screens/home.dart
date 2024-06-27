725b9c97d4ce7e2dd1f049f8fdb483e44d21f8e1
rajendratechment <rajendra.verma@techment.com>
2024-06-27 12:17:45 +0530

    example of simple list

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,23 +4,27 @@ class HomeScreen extends StatelessWidget {
   final String title;
 
   const HomeScreen({super.key, required this.title});
+  
 
   @override
   Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: Center(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-              SizedBox(
-                width: double.infinity,
-                height: 250,
-                child: Image.asset('assets/img.png'),
-              ),
-              Center(child: const Text("Welcome to home screen"))
-            ])));
+        body:  ListView.builder(
+    padding: const EdgeInsets.all(8),
+    itemCount: entries.length,
+    itemBuilder: (BuildContext context, int index) {
+    return Container(
+    height: 50,
+    color: Colors.amber[colorCodes[index]],
+    child: Center(child: Text('Entry ${entries[index]}')),
+    );
+    }
+    ));
+
   }
 }
